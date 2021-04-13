3da328ea524deda462fe18245e8e43b2a344d054
Jacob Anderson <jacobdanderson@gmail.com>
2021-04-12 21:19:04 -0600

    Adding to github

---
+++ pagliaccio.audiot.info/front-end/src/components/Admin.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="main">
+  <h2>Logged in as: {{user.firstName}} {{user.lastName}} <button @click="logout" class="pure-button pure-button-primary">Logout</button></h2>
+  <h1>Administration</h1>
+  <div v-for="ticket in tickets" v-bind:key="ticket.id">
+    <Ticket :ticket="ticket" />
+  </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+import Ticket from '@/components/Ticket.vue';
+export default {
+  name: 'Admin',
+  components: {
+    Ticket
+  },
+  data() {
+    return {
+      tickets: [],
+    }
+  },
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    },
+  },
+  created() {
+    this.getTickets();
+  },
+  methods: {
+    async logout() {
+      try {
+        await axios.delete("/api/users");
+        this.$root.$data.user = null;
+      } catch (error) {
+        this.$root.$data.user = null;
+        console.log("Error in Admin.vue logout()");
+      }
+    },
+    async getTickets() {
+      try {
+        let response = await axios.get("/api/tickets");
+        this.tickets = response.data.tickets;
+        return true;
+      } catch (error) {
+        console.log("Error in Admin.vue getTickets()");
+        console.log(error);
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
